c73ba095152b65840ee9205d19ea60be3873669e
Koyasikitakummi <20233132@kcska.onmicrosoft.com>
2025-11-27 12:16:51 +0900

    ユーザー情報変更・確認・完了画面完成(仮)　修正

--- app/src/main/java/com/example/paceface/ExpressionChangeCompleteScreenActivity.kt
+++ app/src/main/java/com/example/paceface/ExpressionChangeCompleteScreenActivity.kt
@@ -24,7 +24,16 @@ class ExpressionChangeCompleteScreenActivity : AppCompatActivity() {
                 // FLAG_ACTIVITY_SINGLE_TOP: すでに存在する場合は新しいインスタンスを作らない
                 flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
             }
-            startActivity(intent)
+            setContentView(R.layout.expression_change_complete_screen)
+
+            val btnOk = findViewById<Button>(R.id.btn_ok)
+
+            // OK ボタン押下時の処理
+            btnOk.setOnClickListener {
+                // ホーム画面へ遷移
+                val intent = Intent(this, HomeScreenActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
